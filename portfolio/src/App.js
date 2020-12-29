08e3dc88315438f47f68a2cbca24c58501b62345
Dilev <github@dilkovak.com>
2020-12-29 16:51:07 +0100

    switch to js first, before going crazy with ts

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import logo from './logo.svg';
+import './App.css';
 
-function test() {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -23,4 +22,4 @@ function test() {
   );
 }
 
-export default test;
+export default App;
