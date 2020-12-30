4f72ef96c6798e23034c8e3cf66535a84d15cc27
Dilev <github@dilkovak.com>
2020-12-30 14:46:32 +0100

    add template and test

---
+++ portfolio/src/TemplateStructure/Template.js
@@ -0,0 +1,15 @@
+class Template extends React.Component {
+    constructor(props) {    
+        super(props);    
+        this.state = {      
+            // parameters to be used in the state here 
+        };  
+    }
+
+    render() {
+      return (
+          <div></div>
+        // return html here
+      );
+    }
+  }
